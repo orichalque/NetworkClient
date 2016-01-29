5e28a3d020eed14c21954030cd6d62c62739f231
orichalque <thiblf@gmail.com>
2016-01-29 11:07:29 +0100

    folder created

---
+++ SublimTelegram/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: E104607D
+ *
+ * Created on 29 janvier 2016, 11:06
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
