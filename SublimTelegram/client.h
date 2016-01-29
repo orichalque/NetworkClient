1aeaea3f92f37dfca43b0370e0a1f99218b767a4
orichalque <thiblf@gmail.com>
2016-01-29 11:09:26 +0100

    files created

---
+++ SublimTelegram/client.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   client.h
+ * Author: E104607D
+ *
+ * Created on 29 janvier 2016, 11:08
+ */
+
+#ifndef CLIENT_H
+#define	CLIENT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CLIENT_H */
+
---
+++ SublimTelegram/server.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   server.h
+ * Author: E104607D
+ *
+ * Created on 29 janvier 2016, 11:09
+ */
+
+#ifndef SERVER_H
+#define	SERVER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SERVER_H */
+
